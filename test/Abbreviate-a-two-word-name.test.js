c4da9107f4692d5fb5533e208d4becf27dbe6c1f
folorad <fduang@gmail.com>
2023-05-10 15:57:32 +0100

    first commit

---
+++ test/Abbreviate-a-two-word-name.test.js
@@ -0,0 +1,21 @@
+const abbrevName = require("../../codewars-katas/src/8kyu/Abbreviate-a-two-word-name.js");
+const chai = require('chai');
+const assert = chai.assert;
+
+describe("Abbreviate A Two Word Name Tests", () => {
+    it("Testing for Sam Harris", () => {
+        assert.strictEqual(abbrevName("Sam Harris"), "S.H")
+    });
+
+    it("Testing for Patrick Feenan", () => {
+        assert.strictEqual(abbrevName("Patrick Feenan"), "P.F")
+    });
+
+    it("Testing for Evan Cole", () => {
+        assert.strictEqual(abbrevName("Evan Cole"), "E.C")
+    });
+
+    it("Testing for P Favuzzi", () => {
+        assert.strictEqual(abbrevName("P Favuzzi"), "P.F")
+    });
+});
