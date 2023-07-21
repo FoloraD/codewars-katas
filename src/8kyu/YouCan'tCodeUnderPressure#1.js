9d42128496d29fa03cd8d0899e88d3c6a39905e0
folorad <fduang@gmail.com>
2023-07-21 16:24:18 +0100

    Add test YouCan'tCodeUnderPressure#1 kata

---
+++ src/8kyu/YouCan'tCodeUnderPressure#1.js
@@ -0,0 +1,8 @@
+/* 
+You Can't Code Under Pressure #1
+
+DESCRIPTION:
+
+Code as fast as you can! You need to double the integer and return it.
+
+*/
---
+++ test/8kyu/YouCan'tCodeUnderPressure#1.test.js
@@ -0,0 +1,7 @@
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("doubles the integer 2", () => {
+Test.assertEquals(doubleInteger(2), 4);
+  });
+});
